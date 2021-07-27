a477dac8cf2fb14d9d560379d90e4fdd46ea0e74
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-27 01:51:44 -0500

    commit tree 09a9220b69e321096a9c6edd149b915a4869e0ea

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function superbowlWin(arrayOfObj){
+    let supBowl = arrayOfObj.find(function(obj) {
+        return obj.result === "W"
+    })
 
+    debugger
+
+    if (supBowl === undefined) {
+        return supBowl
+    } else {
+        return supBowl["year"]
+    }
+}
